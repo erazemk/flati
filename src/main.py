e731be1da996c2ca724d26d4d967d8830f4b4710
Erazem Kokot <mail@erazem.eu>
2022-08-10 17:47:31 +0200

    Add ability to uninstall apps and clean up

--- src/main.py
+++ src/main.py
@@ -14,67 +14,75 @@ def get_installed_applications() -> list[Flatpak.InstalledRef]:
 
 remotes = installation.list_remotes()
 for remote in remotes:
-    print(remote.get_name())
 
     try:
         installation.update_remote_sync(remote.get_name(), None)
     except GLib.Error as err:
         print(err)
-    else:
-        print("Updated remote {}".format(remote.get_name()))
 
 
 class Flati:
     def __init__(self):
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("flati.ui")
+        self.builder.add_from_file("window.ui")
         self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("window")
+        self.window_stack = self.builder.get_object("window_stack")
+        self.empty_window = self.builder.get_object("empty_window")
+        self.list_window = self.builder.get_object("list_window")
 
         # Either open new installation window or a list of installed applications
         if get_installed_applications() is []:
-            self.window = self.builder.get_object("initial_window")
+            self.window_stack.set_visible_child(self.empty_window)
         else:
-            self.window = self.builder.get_object("main_window")
+            self.window_stack.set_visible_child(self.list_window)
             self.fill_list_box()
 
         self.window.show_all()
 
     def fill_list_box(self):
         listbox = self.builder.get_object("app_list_box")
+        for child in listbox.get_children():
+            listbox.remove(child)
 
         # Fill list with installed applications
         for flatpak in get_installed_applications():
-            builder2 = Gtk.Builder()
-            builder2.add_from_file("flati.ui")
-            row = builder2.get_object("box_row")
+            row_builder = Gtk.Builder()
+            row_builder.add_from_file("window.ui")
+            row_builder.connect_signals(self)
 
-            app_name = builder2.get_object("app_name")
+            row = row_builder.get_object("box_row")
+
+            # app_icon = self.builder.get_object("app_icon")
+
+            app_name = row_builder.get_object("app_name")
             app_name.set_property("label", flatpak.get_appdata_name())
 
-            app_summary = builder2.get_object("app_summary")
+            app_summary = row_builder.get_object("app_summary")
             app_summary.set_property("label", flatpak.get_appdata_summary())
 
-            app_version = builder2.get_object("app_version")
+            app_version = row_builder.get_object("app_version")
             app_version.set_property("label", flatpak.get_appdata_version())
 
-            app_size = builder2.get_object("app_size")
+            app_size = row_builder.get_object("app_size")
             app_size.set_property("label", str(round(flatpak.get_installed_size() / 1000000.0, 1)) + " MB")
 
-            #button = builder2.get_object("uninstall_button")
-            #button.connect("pressed", self.uninstall_flatpak(flatpak))
+            uninstall_button = row_builder.get_object("uninstall_button")
+            uninstall_button.connect("clicked", self.uninstall_flatpak, flatpak)
 
             listbox.add(row)
 
     def on_open_dialog(self, widget):
-        dialog = self.builder.get_object("file_chooser_dialog")
+        dialog = self.builder.get_object("file_chooser")
         response = dialog.run()
 
-        if response == Gtk.ResponseType.OK:
+        if response == Gtk.ResponseType.ACCEPT:
             path = dialog.get_filename()
-            dialog.close()
+            dialog.hide()
             self.install_flatpak(path)
         else:
-            dialog.close()
+            print("No file selected")
 
     def on_close_dialog(self, widget, event):
         return self.builder.get_object("file_chooser_dialog").hide_on_delete()
@@ -93,38 +101,36 @@ class Flati:
             except GLib.Error as err:
                 print(err)
             else:
+                print("Installed " + path)
                 if first_install:
                     # Replace original window with list of applications
-                    self.window.close()
-                    self.window = self.builder.get_object("main_window")
+                    self.window_stack.set_visible_child(self.list_window)
                     self.fill_list_box()
-                    self.window.show_all()
                 else:
                     # Update list of applications
                     self.fill_list_box()
-                    self.window.show_all()
 
-    def uninstall_flatpak(self, ref):
+    def uninstall_flatpak(self, obj, ref):
         print("Pressed uninstall button for " + ref.get_appdata_name())
         try:
             transaction = Flatpak.Transaction.new_for_installation(installation, None)
             transaction.add_uninstall(ref.format_ref())
-            #transaction.run()
+            transaction.run()
         except GLib.Error as err:
             print(err)
         else:
+            print("Uninstalled " + ref.get_appdata_name())
             first_install = True if get_installed_applications() is [] else False
 
             if first_install:
                 # Replace original window with list of applications
-                self.window.close()
-                self.window = self.builder.get_object("main_window")
-                self.fill_list_box()
-                self.window.show_all()
+                self.window_stack.set_visible_child(self.empty_window)
             else:
                 # Update list of applications
+                self.window_stack.set_visible_child(self.list_window)
                 self.fill_list_box()
-                self.window.show_all()
+
+            self.window.show_all()
 
 
 Flati()
