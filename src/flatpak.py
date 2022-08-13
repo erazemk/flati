6f4730ada1c6d3e79fefaada9a34abc3757e3a51
Erazem Kokot <mail@erazem.eu>
2022-08-13 21:22:49 +0200

    Update UI and add list of updatable apps

--- src/flatpak.py
+++ src/flatpak.py
@@ -6,6 +6,7 @@ from gi.repository import GLib, Flatpak
 from os import path
 
 installation = Flatpak.get_system_installations()[0]
+arch = Flatpak.get_default_arch()
 remotes = installation.list_remotes()
 
 
@@ -18,17 +19,38 @@ def sync_remotes():
             print(err)
 
 
-def installed_applications() -> list[Flatpak.InstalledRef]:
+def update_appstream(remote):
+    """Updates the local copy of appstream for remote"""
+    print("Updating appstream for " + remote)
+    try:
+        installation.update_appstream_full_sync(remote.get_name(), arch, None, None, None)
+    except GLib.Error as err:
+        print(err)
+    else:
+        print("Updated appstream for " + remote)
+
+
+def get_updates() -> list[Flatpak.InstalledRef]:
+    """Returns a list of available updates"""
+    return installation.list_installed_refs_for_update(None)
+
+
+def installed_apps() -> list[Flatpak.InstalledRef]:
     """Returns a list of installed applications"""
     return installation.list_installed_refs_by_kind(Flatpak.RefKind.APP, None)
 
 
-def number_of_installed_applications():
+def num_of_installed_apps():
     """Returns the number of installed applications"""
-    return len(installed_applications())
+    return len(installed_apps())
 
 
-def install(file):
+def num_of_updatable_apps():
+    """Returns the number of updatable applications"""
+    return len(get_updates())
+
+
+def install_app(file):
     """Installs an application from a ref file"""
     filename = path.basename(file)
     print("Installing " + filename)
@@ -46,7 +68,26 @@ def install(file):
             print("Installed " + filename)
 
 
-def uninstall(ref):
+def update_app(ref):
+    """Updates an application"""
+    print("Updating " + ref.get_appdata_name())
+    try:
+        transaction = Flatpak.Transaction.new_for_installation(installation, None)
+        transaction.add_update(ref.format_ref(), None, None)
+        transaction.run()
+    except GLib.Error as err:
+        print(err)
+    else:
+        print("Updated " + ref.get_appdata_name())
+
+
+def update_all_apps():
+    """Updates all installed applications"""
+    for app in get_updates():
+        update_app(app)
+
+
+def uninstall_app(ref):
     """Uninstalls an application"""
     print("Uninstalling " + ref.get_appdata_name())
     try:
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-import flatpak
 import gi
+
+import flatpak
 
 gi.require_version("Gtk", "3.0")
 
@@ -14,14 +15,15 @@ class Flati:
 
         self.window = self.builder.get_object("window")
         self.window_stack = self.builder.get_object("window_stack")
-        self.empty_window = self.builder.get_object("empty_window")
         self.list_window = self.builder.get_object("list_window")
+        self.app_list_button = self.builder.get_object("app_list_button")
 
-        # Either open new installation window or a list of installed applications
-        if flatpak.number_of_installed_applications() == 0:
-            self.window_stack.set_visible_child(self.empty_window)
+        self.window_stack.connect("notify::visible-child", self.on_stack_visible_child_switch)
+
+        if flatpak.num_of_installed_apps() == 0:
+            # TODO: Add a label saying there are no installed applications
+            pass
         else:
-            self.window_stack.set_visible_child(self.list_window)
             self.fill_list_box()
 
         self.window.show_all()
@@ -31,12 +33,22 @@ class Flati:
         for child in listbox.get_children():
             listbox.remove(child)
 
-        # Fill list with installed applications
-        for app in flatpak.installed_applications():
+        window_name = self.window_stack.get_visible_child_name()
+
+        app_list = []
+        if window_name == "installed-apps":
+            # Fill list with installed applications
+            app_list = flatpak.installed_apps()
+        elif window_name == "updatable-apps":
+            # Fill list with updatable applications
+            app_list = flatpak.get_updates()
+        else:
+            print("Unknown window name: " + window_name)
+
+        for app in app_list:
             row_builder = Gtk.Builder()
             row_builder.add_from_file("applist.ui")
             row_builder.connect_signals(self)
-
             row = row_builder.get_object("app_row")
 
             # app_icon = self.builder.get_object("app_icon")
@@ -53,42 +65,73 @@ class Flati:
             app_size = row_builder.get_object("app_size")
             app_size.set_property("label", str(round(app.get_installed_size() / 1000000.0, 1)) + " MB")
 
-            uninstall_button = row_builder.get_object("uninstall_button")
-            uninstall_button.connect("clicked", self.on_uninstall_button_clicked, app)
+            app_button = row_builder.get_object("app_button")
+            app_button.connect("clicked", self.on_app_button_clicked, app)
 
             listbox.add(row)
 
-    def on_install_button_clicked(self, widget):
-        dialog = self.builder.get_object("file_chooser")
-        response = dialog.run()
+    def on_stack_visible_child_switch(self, widget, param):
+        """Updates app list button label based on the visible child of the stack"""
+        window_name = self.window_stack.get_visible_child_name()
 
-        if response == Gtk.ResponseType.ACCEPT:
-            path = dialog.get_filename()
-            dialog.hide()
-            flatpak.install(path)
+        if window_name == "installed-apps":
+            self.app_list_button.set_label("Install new")
+        elif window_name == "updatable-apps":
+            self.app_list_button.set_label("Update all")
+        else:
+            print("Unknown window name: " + window_name)
 
-            if flatpak.number_of_installed_applications() == 0:
-                # Replace original window with list of applications
-                self.window_stack.set_visible_child(self.list_window)
-                self.fill_list_box()
+        self.fill_list_box()
+
+    def on_app_list_button_clicked(self, widget):
+        """Either installs a new application from .flatpakref or updates all installed applications"""
+        window_name = self.window_stack.get_visible_child_name()
+
+        if window_name == "installed-apps":
+            # Install new application
+            dialog = self.builder.get_object("file_chooser")
+            response = dialog.run()
+
+            if response == Gtk.ResponseType.ACCEPT:
+                file = dialog.get_filename()
+                dialog.hide()
+                flatpak.install_app(file)
+
+                if flatpak.num_of_installed_apps() == 0:
+                    # TODO: Add a label saying there are no installed applications
+                    pass
+                else:
+                    self.fill_list_box()
             else:
-                # Update list of applications
+                print("No file selected")
+        elif window_name == "updatable-apps":
+            # Update all applications
+            flatpak.update_all_apps()
+        else:
+            print("Unknown window name: " + window_name)
+
+    def on_app_button_clicked(self, obj, ref):
+        """Either uninstalls an application or updates it"""
+        window_name = self.window_stack.get_visible_child_name()
+
+        if window_name == "installed-apps":
+            flatpak.uninstall_app(ref)
+
+            if flatpak.num_of_installed_apps() == 0:
+                # TODO: Add a label saying there are no installed applications
+                pass
+            else:
+                self.fill_list_box()
+        elif window_name == "updatable-apps":
+            flatpak.update_app(ref)
+
+            if flatpak.num_of_updatable_apps() == 0:
+                # TODO: Add a label saying there are no updatable applications
+                pass
+            else:
                 self.fill_list_box()
         else:
-            print("No file selected")
-
-    def on_uninstall_button_clicked(self, obj, ref):
-        flatpak.uninstall(ref)
-
-        if flatpak.number_of_installed_applications() == 0:
-            # Replace original window with empty window
-            self.window_stack.set_visible_child(self.empty_window)
-        else:
-            # Update list of applications
-            self.window_stack.set_visible_child(self.list_window)
-            self.fill_list_box()
-
-        self.window.show_all()
+            print("Unknown window name: " + window_name)
 
     def on_close_dialog(self, widget, event):
         return self.builder.get_object("file_chooser_dialog").hide_on_delete()
