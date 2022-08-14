00a72d5d79db61862eb63e58de0e7bed741d43a6
Erazem Kokot <mail@erazem.eu>
2022-08-14 13:36:01 +0200

    Use match instead of if-elif-else

--- src/main.py
+++ src/main.py
@@ -32,16 +32,17 @@ class Flati:
         app_list = []
 
         # Fill application list based on visible window
-        if window_name == "installed-apps":
-            app_list = flatpak.get_installed_apps()
-            listbox = self.builder.get_object("app_list_box")
-            no_apps_label_markup = "<span size='large' weight='bold'>No installed applications</span>"
-        elif window_name == "updatable-apps":
-            app_list = flatpak.get_updates()
-            listbox = self.builder.get_object("updates_list_box")
-            no_apps_label_markup = "<span size='large' weight='bold'>All applications are up-to-date</span>"
-        else:
-            logging.error("Unknown window name: " + window_name)
+        match window_name:
+            case "installed-apps":
+                app_list = flatpak.get_installed_apps()
+                listbox = self.builder.get_object("app_list_box")
+                no_apps_label_markup = "<span size='large' weight='bold'>No installed applications</span>"
+            case "updatable-apps":
+                app_list = flatpak.get_updates()
+                listbox = self.builder.get_object("updates_list_box")
+                no_apps_label_markup = "<span size='large' weight='bold'>All applications are up-to-date</span>"
+            case _:
+                logging.error("Unknown window name: " + window_name)
 
         # Remove previous listed applications
         for child in listbox.get_children():
@@ -98,12 +99,13 @@ class Flati:
         """Updates app list button label based on the visible child of the stack"""
         window_name = self.window_stack.get_visible_child_name()
 
-        if window_name == "installed-apps":
-            self.app_list_button.set_label("Install new")
-        elif window_name == "updatable-apps":
-            self.app_list_button.set_label("Update all")
-        else:
-            logging.error("Unknown window name: " + window_name)
+        match window_name:
+            case "installed-apps":
+                self.app_list_button.set_label("Install new")
+            case "updatable-apps":
+                self.app_list_button.set_label("Update all")
+            case _:
+                logging.error("Unknown window name: " + window_name)
 
         self.fill_list_box()
 
@@ -111,36 +113,38 @@ class Flati:
         """Either installs a new application from .flatpakref or updates all installed applications"""
         window_name = self.window_stack.get_visible_child_name()
 
-        if window_name == "installed-apps":
-            # Install new application
-            dialog = self.builder.get_object("file_chooser")
-            response = dialog.run()
+        match window_name:
+            case "installed-apps":
+                # Install new application
+                dialog = self.builder.get_object("file_chooser")
+                response = dialog.run()
 
-            if response == Gtk.ResponseType.ACCEPT:
-                file = dialog.get_filename()
-                dialog.hide()
-                flatpak.install_app(file)
-                self.fill_list_box()
-            else:
-                logging.debug("No file selected")
-        elif window_name == "updatable-apps":
-            # Update all applications
-            flatpak.update_all_apps()
-        else:
-            logging.error("Unknown window name: " + window_name)
+                if response == Gtk.ResponseType.ACCEPT:
+                    file = dialog.get_filename()
+                    dialog.hide()
+                    flatpak.install_app(file)
+                    self.fill_list_box()
+                else:
+                    logging.debug("No file selected")
+            case "updatable-apps":
+                # Update all applications
+                flatpak.update_all_apps()
+            case _:
+                logging.error("Unknown window name: " + window_name)
 
     def on_app_button_clicked(self, obj, ref):
         """Either uninstalls an application or updates it"""
         window_name = self.window_stack.get_visible_child_name()
 
-        if window_name == "installed-apps":
-            flatpak.uninstall_app(ref)
-            self.fill_list_box()
-        elif window_name == "updatable-apps":
-            flatpak.update_app(ref)
-            self.fill_list_box()
-        else:
-            logging.error("Unknown window name: " + window_name)
+        match window_name:
+            case "installed-apps":
+                flatpak.uninstall_app(ref)
+                self.fill_list_box()
+            case "updatable-apps":
+                flatpak.update_app(ref)
+                self.fill_list_box()
+            case _:
+                logging.error("Unknown window name: " + window_name)
 
     def on_close_dialog(self, widget, event):
         return self.builder.get_object("file_chooser_dialog").hide_on_delete()
